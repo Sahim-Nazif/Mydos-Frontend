c7d80c319f6f617d52b4fbf9dd7bf2f81a071025
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2022-06-23 22:29:24 -0400

    created routes

---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BrowserRouter, Routes as Router, Route} from 'react-router-dom'
+import Home from './components/Home'
+
+const Routes = () => {
+  return (
+     <BrowserRouter>
+        <Router>
+            <Route  exact path='/' element={<Home />} />
+
+        </Router>
+     
+     </BrowserRouter>
+    
+  )
+}
+
+export default Routes
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+const Home = () => {
+  return (
+    <div className='container'>
+    <Header/>
+     <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home
